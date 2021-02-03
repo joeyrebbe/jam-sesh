369dd6e459f7c67ec0cc4b5e46d135c224cf560e
joeyrebbe <josephrebbe@gmail.com>
2021-02-03 01:18:54 -0800

    postCard styling, added route to search

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import GuitarlImg from '../assets/guitar.png'
-import GuitarlPath from '../assets/path.svg'
+import GuitarImg from '../assets/guitar.png'
+import GuitarPath from '../assets/path.svg'
 import Hero from '../components/Hero'
 import '../styles/Landing.css'
 import Nav from '../components/Nav'
@@ -11,21 +11,20 @@ export default function LandingPage ({ children }) {
       <section className="left flex-sm flex-col">
         <div className="mask flex-col">
           <div className="content-wrapper flex-col">
-            <h3 className="logo">Logo</h3>
             <div className="hero-wrapper flex-row">
               <div className="cl-left flex-col">
                 <Hero />
               </div>
               <div className="cl-right flex-col">
                 <div className="path-wrapper">
-                  <img src={GuitarlPath} alt="path" />
+                  <img src={GuitarPath} alt="path" />
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="img-wrapper">
-          <img src={GuitarlImg} alt="water" />
+          <img src={GuitarImg} />
         </div>
       </section>
       <section className="right flex-sm">
