50587efaf391bbcfd6f5e877c6a3a05146bb3928
joeyrebbe <josephrebbe@gmail.com>
2021-02-04 16:40:57 -0800

    fixing deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const AppRouter = require('./routes/AppRouter')
 const express = require('express')
 const logger = require('morgan')
@@ -16,7 +17,6 @@ app.use(helmet({ contentSecurityPolicy: false }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(favicon(path.join(__dirname, 'build', 'client', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 // Initialize Middleware
 app.disable('X-Powered-By')
